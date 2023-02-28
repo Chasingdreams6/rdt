647faba7f4e6453529b02fc8376642c6e05d042c
hanjinbo <hanjinbo2002@gmail.com>
2023-02-28 16:38:29 +0800

    [fix] fix stop timer bug. Now, each seq number will only have exactly one timer. And after every physical timeout, there may be multiple logical timer which "timeout".

--- rdt_receiver.cc
+++ rdt_receiver.cc
@@ -24,6 +24,7 @@
 
 std::map<int, packet> buffered_packets;
 int cur_seq_expected = 0;
+int tot_to = 0;
 /* receiver initialization, called once at the very beginning */
 void Receiver_Init()
 {
@@ -46,7 +47,7 @@ void Receiver_Final()
 void Receiver_FromLowerLayer(struct packet *pkt)
 {
     if (!check_packet(pkt)) { // wrong packet, ignore it
-        DEBUG("Corrupted packet!\n", 2);
+        DEBUG("[Receiver]Corrupted packet!\n", 2);
         return ;
     } else {
         if (pkt->data[1] == cur_seq_expected) { // right order
@@ -57,21 +58,22 @@ void Receiver_FromLowerLayer(struct packet *pkt)
             Receiver_ToUpperLayer(msg);
             inc(cur_seq_expected);
             //cur_seq_expected = (cur_seq_expected + 1) % (MAX_SEQ + 1);
-            DEBUG("Receiver received seq = %d, send ack to sender\n", pkt->data[1]);
+            DEBUG("[LOWER] got seq = %d, tot_to = %d\n", pkt->data[1], ++tot_to);
+            DEBUG("[RR]Receiver received seq = %d, send ack to sender\n", pkt->data[1]);
             Receiver_ToLowerLayer(pkt); // ack
             if (msg->data != NULL) free(msg->data);
             if (msg != NULL)free(msg);
         } else {
             // current turn's packet
             if (this_turn(pkt->data[1], cur_seq_expected)) {
-                DEBUG("Receiver receive seq = %d, but expect %d, store it\n", pkt->data[1], cur_seq_expected);
+                DEBUG("[RR-O]Receiver receive seq = %d, but expect %d, store it and ack\n", pkt->data[1], cur_seq_expected);
                 if (buffered_packets.count(pkt->data[1])) {
                     DEBUG("****Fatal: buffer overflow seq=%d\n", pkt->data[1]);
                 }
                 buffered_packets[pkt->data[1]] = *pkt;
             }
             else {
-                DEBUG("Receiver receive seq = %d, but expect %d, and this may be last turn, ignore it\n",
+                DEBUG("[RR-L]Receiver receive seq = %d, but expect %d, and this may be last turn, only send ack back\n",
                       pkt->data[1], cur_seq_expected);
             }
             Receiver_ToLowerLayer(pkt); // send ack this packet
@@ -83,7 +85,8 @@ void Receiver_FromLowerLayer(struct packet *pkt)
             msg->data = (char*) malloc (msg->size);
             memcpy(msg->data, pkt.data + HEADER_SIZE, msg->size);
             Receiver_ToUpperLayer(msg);
-            DEBUG("Receiver from buffer, get seq=%d\n", cur_seq_expected);
+            DEBUG("[LOWER] got seq = %d, total to = %d\n", cur_seq_expected, ++tot_to);
+            DEBUG("[RR-B]Receiver from buffer, get seq=%d\n", cur_seq_expected);
             ASSERT(buffered_packets.erase(cur_seq_expected) > 0);
             inc(cur_seq_expected);
             //Receiver_ToLowerLayer(&pkt); // ack
--- rdt_sender.cc
+++ rdt_sender.cc
@@ -24,11 +24,16 @@
 #include "rdt_sender.h"
 #include "rdt_util.h"
 
+int tot_from = 0;
 int next_frame_to_send, ack_expected, buffered_num;
 std::queue<packet> packets;
 std::list<Time_pair> logical_clock;
+bool clock_set[MAX_SEQ + 1];
+//std::map<double, Time_pair> logical_clock;
 packet buffers[MAX_SEQ + 1];
 bool buffered_ack[MAX_SEQ + 1];
+
+void resendPacket(int seq);
 bool push_to_buffer(packet &packet) {
     packets.push(packet);
     return true;
@@ -43,17 +48,42 @@ bool pop_from_buffer(packet *pPacket) {
 
 
 void Wrapped_StartTimer(int seq) {
-    DEBUG("[TIMER] add seq=%d to timer\n", seq);
+    //DEBUG("[TIMER] add seq=%d to timer\n", seq);
+    if (clock_set[seq]) return ;
+    clock_set[seq] = true;
+    double time = GetSimulationTime();
     if (Sender_isTimerSet())
-        logical_clock.push_back(Time_pair(GetSimulationTime(), seq));
+        logical_clock.push_back(Time_pair(time, seq));
     else {
-        logical_clock.push_back(Time_pair(GetSimulationTime(), seq));
+        logical_clock.push_back(Time_pair(time, seq));
         Sender_StartTimer(TIMEOUT);
     }
 }
-
+void Update_clock() {
+    std::vector<int> resend_list;
+    while (!logical_clock.empty()) { // start a new timer from the list head
+        double lastTime = (*logical_clock.begin()).create_time + TIMEOUT - GetSimulationTime();
+        int seq = (*logical_clock.begin()).seq;
+        if (lastTime <= 0) { // already expired
+            DEBUG("Clock seq = %d, create_time = %f expired\n", seq, (*logical_clock.begin()).create_time);
+            clock_set[seq] = false;
+            resend_list.emplace_back(seq);
+            logical_clock.erase(logical_clock.begin());
+        } else {
+            Sender_StartTimer(lastTime);
+            break;
+        }
+    }
+    if (logical_clock.empty()) { // no timer current
+        Sender_StopTimer();
+    }
+    for (auto x : resend_list) { // fix some packages
+        resendPacket(x);
+    }
+}
 void Wrapped_StopTimer(int seq) {
-    DEBUG("[TIMER]stop timer seq=%d\n", seq);
+    //DEBUG("[TIMER]stop timer seq=%d\n", seq);
+    clock_set[seq] = false;
     bool flag = false;
     for (auto it = logical_clock.begin(); it != logical_clock.end(); ++it) {
         if ((*it).seq == seq) {
@@ -64,28 +94,15 @@ void Wrapped_StopTimer(int seq) {
             break;
         }
     }
-    if (flag) {
-        while (!logical_clock.empty()) {
-            double lastTime = (*logical_clock.begin()).create_time + TIMEOUT - GetSimulationTime();
-            if (lastTime <= 0) { // give up this clock, rely on last resend's
-                DEBUG("Clock seq = %d, create_time = %f expired\n", (*logical_clock.begin()).seq
-                      , (*logical_clock.begin()).create_time);
-                logical_clock.erase(logical_clock.begin());
-            } else {
-                Sender_StartTimer(lastTime);
-                break;
-            }
-        }
-        if (logical_clock.empty()) { // no timer current
-            Sender_StopTimer();
-            return ;
-        }
+    if (flag) { // may no timer
+        Update_clock();
     }
 }
 
 // try to send a packet, if buffered_num >= 10, directly return
 // this will be called everywhere
 void try_sendPacket() {
+    //if (GetSimulationTime() > 20) abort();
     if (buffered_num >= MAX_WINDOW) return; // inque number, waiting for ack ...
     packet pkt;
     if (!pop_from_buffer(&pkt)) return;
@@ -100,7 +117,7 @@ void resendPacket(int seq) {
     packet pkt = buffers[seq];
     Wrapped_StartTimer(seq);
     Sender_ToLowerLayer(&pkt);
-    DEBUG("Resend a packet, seq = %d\n", seq);
+    DEBUG("Resent a packet, seq = %d\n", seq);
 }
 
 /* sender initialization, called once at the very beginning */
@@ -135,6 +152,7 @@ void Sender_FromUpperLayer(struct message *msg) {
         if (!push_to_buffer(pkt)) {
             DEBUG("Fatal: buffer used up, seq = %d\n", next_frame_to_send);
         }
+        DEBUG("[UPPER]: got seq = %d, total = %d\n", pkt.data[1], ++tot_from);
         cursor += maxpayload_size;
         try_sendPacket();
     }
@@ -144,6 +162,7 @@ void Sender_FromUpperLayer(struct message *msg) {
         inc(next_frame_to_send);
         memcpy(pkt.data + HEADER_SIZE, msg->data + cursor, msg->size - cursor);
         build_checksum(&pkt);
+        DEBUG("[UPPER]: got seq = %d, total = %d\n", pkt.data[1], ++tot_from);
         if (!push_to_buffer(pkt)) {
             DEBUG("Fatal: buffer used up, seq = %d\n", next_frame_to_send);
         }
@@ -158,43 +177,38 @@ void Sender_FromUpperLayer(struct message *msg) {
    */
 void Sender_FromLowerLayer(struct packet *pkt) {
     if (!check_packet(pkt)) {
-        DEBUG("Corrupted packet!\n", 1);
+        DEBUG("[Sender]Corrupted packet!\n", 1);
         return ;
     }
     if (ack_expected == pkt->data[1]) { // success to receive ack
         Wrapped_StopTimer(ack_expected);
         buffered_num--;
         inc(ack_expected);
-        DEBUG("Sender received ack, seq = %d, is expected\n", pkt->data[1]);
-        try_sendPacket();
+        DEBUG("[S-ACK]Sender received ack, seq = %d, is expected\n", pkt->data[1]);
     } else { // another packet's ack, just stop the timer
         if (this_turn(pkt->data[1], ack_expected)) {
-            DEBUG("Sender received ack, seq = %d, not expected, store it\n", pkt->data[1]);
+            DEBUG("[S-O]Sender received ack, seq = %d, not expected, store it\n", pkt->data[1]);
             if (buffered_ack[pkt->data[1]]) {
                 DEBUG("FATAL: *** buffered_ack overwritted, seq = %d\n", pkt->data[1]);
             }
             buffered_ack[pkt->data[1]] = true;
         }
         else
-            DEBUG("Sender received ack, seq = %d, not expected and too old, ignore it\n", pkt->data[1]);
+            DEBUG("[S-L]Sender received ack, seq = %d, not expected and too old, ignore it\n", pkt->data[1]);
         Wrapped_StopTimer(pkt->data[1]); // even it's old, should stop timer
     }
     while (buffered_ack[ack_expected]) {
         buffered_num--;
-        DEBUG("Sender get ack from buffer, seq %d\n", ack_expected);
+        DEBUG("[S-B]Sender get ack from buffer, seq %d\n", ack_expected);
         buffered_ack[ack_expected] = false;
         inc(ack_expected);
-        try_sendPacket();
     }
+    try_sendPacket();
 }
 
 /* event handler, called when the timer expires */
 /* simply resend all datas in buffer*/
 void Sender_Timeout() {
-    DEBUG("Timeout, seq = %d, resend it\n", ack_expected);
-//    int tmp = buffered_num;
-//    for (int i = 0; i < tmp; ++i) {
-//        resendPacket((ack_expected + i) % (MAX_SEQ + 1));
-//    }
-    resendPacket(ack_expected);
+    DEBUG("Timeout, logical_clock is %d, seq = %d, update_clock\n", (*logical_clock.begin()).seq, ack_expected);
+    Update_clock();
 }
